49cc74275930f6b3834caee9c2b3ca5200d2db62
Khoi Tran <emails.khoi@gmail.com>
2014-08-21 13:35:27 -0700

    add precompile

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Rideshare
     config.i18n.enforce_available_locales = false
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
     # Enable the asset pipeline
+    config.assets.enabled = true
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.generators do |g|
       g.test_framework :rspec
