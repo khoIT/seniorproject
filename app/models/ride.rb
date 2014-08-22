aec69cc5a0479a40a276ce73e1bec97b5f06b8ba
Khoi Tran <emails.khoi@gmail.com>
2014-08-22 16:54:18 -0700

    Render in JSON

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,6 +1,6 @@
 class Ride < ActiveRecord::Base
    has_many :passenger_rides, foreign_key: "ride_id", dependent: :destroy
-   has_many :passengers, through: :passenger_rides, source: :passenger, class_name: "User"
+   has_many :passengers, through: :passenger_rides, class_name: "User"
 
    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :rides, through: :passenger_rides, class_name: "Ride"
 
   has_many :fares, foreign_key: "driver_id", class_name: "Ride"
+
   def hopped_in?(ride)
       self.passenger_rides.find_by_ride_id(ride.id)
   end
