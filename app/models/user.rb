df8c5482d93435352882c86119f26a022948c664
Khoi Tran <emails.khoi@gmail.com>
2014-08-23 17:28:52 -0700

    change update_attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   def hop_in!(ride)
      self.rides << ride
-     self.passenger_rides.find_by_ride_id(ride.id).update_attributes(confirmed: "false")
+     self.passenger_rides.find_by_ride_id(ride.id).update_attributes!(confirmed: "false")
   end
 
   def jump_off!(ride)
