8c0de80129b3bcd76741ef95f229af47893feb19
Khoi Tran <emails.khoi@gmail.com>
2014-08-28 18:27:53 -0400

    omniauth works but not the image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
   has_many :passenger_rides, foreign_key: "passenger_id", dependent: :destroy
   has_many :rides, through: :passenger_rides, class_name: "Ride"
 
@@ -46,5 +47,37 @@ class User < ActiveRecord::Base
 
   def is_driver?
     User.where(driver:true).exists?(self)
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  config.omniauth :facebook, "559857074118686", "a892c1eac456baa8af35bc9b79f0476c"
 end
