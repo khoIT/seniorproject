1258c14da878b22bf767eca42c7007594fd35fc3
Khoi Tran <emails.khoi@gmail.com>
2014-08-29 01:33:47 -0400

    444 - login with facebook or google+

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rideshare::Application.config.session_store :cookie_store, key: '_rideshare_session'
+Rideshare::Application.config.session_store :active_record_store
