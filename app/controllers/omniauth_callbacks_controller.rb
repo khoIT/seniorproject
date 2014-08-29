575f456160a9d7aa6dfdc423c3747c753fee7500
Khoi Tran <emails.khoi@gmail.com>
2014-08-29 02:10:11 -0400

    Merge branch 'master' of heroku.com:depauwrides

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def all
+  def google_oauth2
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
       if @user.persisted?
@@ -10,6 +10,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         redirect_to new_user_registration_url, email: session["devise.google_data"].info.email
       end
   end
-  alias_method :google_oauth2, :all
-  alias_method :facebook, :all
+
+  def facebook
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+        sign_in_and_redirect @user, :event => :authentication, email: request.env["omniauth.auth"].info.email
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url, email: session["devise.google_data"].info.email
+      end
+  end
 end
