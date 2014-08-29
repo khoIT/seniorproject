8857b8802fd0909235a1cd4de8422dc845e87556
Khoi Tran <emails.khoi@gmail.com>
2014-08-29 01:37:19 -0400

    444 - login with facebook or google+

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,28 +58,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"], without_protection: true) do |user|
-        user.attributes = params
-        user.valid?
-      end
-    else
-      super
-    end
-  end
-
-  def password_required?
-    super && provider.blank?
-  end
-
-  def update_with_password(params, *options)
-    if encrypted_password.blank?
-      update_attributes(params, *options)
-    else
-      super
-    end
-  end
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data["email"]).first
